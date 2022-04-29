d8cee56e80b902ad49097848451d6670c3081232
root <root@LF-PC.localdomain>
2022-04-30 02:28:41 +0800

    邻接矩阵的广度遍历完成

--- Go/graph/adjancencyMatrix/main.go
+++ Go/graph/adjancencyMatrix/main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 )
 
-const INFINTY = 65535 // 用65535来表示无限大
+const X = 65535 // 用65535来表示无限大
 
 // 定义一个邻接矩阵图
 type MGraph struct {
@@ -33,7 +33,7 @@ func CreateMGraph(m *MGraph) {
 			if i == j {
 				m.arc[i][j] = 0
 			} else {
-				m.arc[i][j] = INFINTY
+				m.arc[i][j] = X
 			}
 		}
 	}
@@ -53,8 +53,84 @@ func CreateMGraph(m *MGraph) {
 	}
 
 }
+
+// 邻接矩阵深度优先递归算法
+func DFS(mg *MGraph, i int, v []bool) {
+	v[i] = true
+	fmt.Printf("mg.vexs[%d]: %v\n", i, mg.vexs[i]) // 打印顶点(也可以做其他操作)
+	for j := 0; j < mg.numVertexes; j++ {
+
+		// 对未访问的邻接顶点递归调用
+		if mg.arc[i][j] > 0 && mg.arc[i][j] < X && !v[j] {
+			DFS(mg, j, v)
+		}
+	}
+}
+
+// 邻接矩阵的深度遍历操作
+func DFSTraverse(mg *MGraph) {
+	vistied := make([]bool, mg.numVertexes) // 访问标记的切片
+	// 对未访问过得顶点调用DFS
+	// 如果是连通图, 只会执行一次(相当于不用for, 直接 DFS(mg, 0, vistied) )
+	for i := 0; i < mg.numVertexes; i++ {
+		if !vistied[i] {
+			DFS(mg, i, vistied)
+		}
+	}
+}
+
+// 邻接矩阵广度优先递归算法
+func BFSTraverse(mg *MGraph) {
+	queue := make([]int, 0)                 // 初始化一个辅助用的队列
+	vistied := make([]bool, mg.numVertexes) // 访问标记的切片
+	// 对每一个顶点做循环, 如果是连通图, 只会执行一次
+	for i := 0; i < mg.numVertexes; i++ {
+		if !vistied[i] {
+			vistied[i] = true                              // 设置当前顶点访问过
+			fmt.Printf("mg.vexs[%d]: %v\n", i, mg.vexs[i]) // 打印顶点, 也可以做其他操作
+			queue = append(queue, i)
+			// 若当前队列不为空
+			for len(queue) > 0 {
+				i = queue[0] // 将队列中的元素取出, 赋值给i
+				queue = queue[1:]
+				for j := 0; j < mg.numVertexes; j++ {
+					// 判断其他顶点若与当前顶点有边存在, 且从未被访问过
+					if mg.arc[i][j] > 0 && mg.arc[i][j] < X && !vistied[j] {
+						vistied[j] = true // 将找到的此顶点标记为已访问
+						fmt.Printf("mg.vexs[%d]: %v\n", j, mg.vexs[j])
+						queue = append(queue, j) // 将找到的此顶点加入队列
+					}
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	mg := new(MGraph)
-	CreateMGraph(mg)
-	fmt.Println(*mg)
+	// mg := new(MGraph)
+	// CreateMGraph(mg)
+	// fmt.Println(*mg)
+	// DFSTraverse(mg)
+
+	mg2 := MGraph{
+		vexs: []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"},
+		arc: [][]int{
+			//A B  C  D  E  F  G  H  I
+			{0, 1, X, X, X, 1, X, X, X}, // A
+			{1, 0, 1, X, X, X, 1, X, 1}, // B
+			{X, 1, 0, 1, X, X, X, X, 1}, // C
+			{X, X, 1, 0, 1, X, 1, 1, 1}, // D
+			{X, X, X, 1, 0, 1, X, 1, X}, // E
+			{1, X, X, X, 1, 0, 1, X, X}, // F
+			{X, 1, X, 1, X, 1, 0, 1, X}, // G
+			{X, X, X, 1, 1, X, 1, 0, X}, // H
+			{X, 1, 1, 1, X, X, X, X, 0}, // I
+		},
+		numVertexes: 9,
+		numEdges:    15,
+	}
+	fmt.Println(mg2)
+	DFSTraverse(&mg2)
+	fmt.Println("==============")
+	BFSTraverse(&mg2)
 }
