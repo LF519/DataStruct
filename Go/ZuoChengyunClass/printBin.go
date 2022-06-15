0f413317adbbe611475af05db63e26f08f4bbd44
root <root@LF-PC.localdomain>
2022-06-15 21:47:30 +0800

    6/25更新, 新增左程云的教程

---
+++ Go/ZuoChengyunClass/printBin.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func print(num int32) {
+	for i := 31; i >= 0; i-- {
+		if num&(1<<i) == 0 {
+			fmt.Print("0")
+		} else {
+			fmt.Print("1")
+		}
+	}
+	fmt.Println()
+}
+
+func main() {
+	var num int32 = 4
+	print(num)
+	print(num << 1)
+	print(num << 2)
+	print(num << 8)
+}
